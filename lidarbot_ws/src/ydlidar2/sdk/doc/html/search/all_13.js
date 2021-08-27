f6de5b52ef04889be8673e13a692f8948d596b2d
yoraish <yorai.shaoul@gmail.com>
2021-08-27 08:45:50 +0000

    Removing old ydlidar files.

--- lidarbot_ws/src/ydlidar2/sdk/doc/html/search/all_13.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['usingspaces',['UsingSpaces',['../class_c_simple_ini_templ.html#a9c967faf796cf5babea67e97975bed9b',1,'CSimpleIniTempl']]]
-];
--- lidarbot_ws/src/ydlidar2/sdk/doc/html/search/classes_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['keyorder',['KeyOrder',['../struct_c_simple_ini_templ_1_1_entry_1_1_key_order.html',1,'CSimpleIniTempl::Entry']]]
-];
